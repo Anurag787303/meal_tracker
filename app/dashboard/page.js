5a43bc998e4f4eaefac0ad6bf5a0bdca8f3745d8
Anurag Meena <anurag31415@gmail.com>
2024-05-06 01:35:12 +0530

    basic dashboard layout and styles

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,6 @@
 import { getUserDetails, isAuthenticated } from "@/lib/actions"
 import { redirect } from "next/navigation"
+import style from './style.module.css'
 
 const Dashboard = async () => {
     const isAuth = await isAuthenticated()
@@ -13,7 +14,34 @@ const Dashboard = async () => {
     }
 
     return (
-        <div>{data.username}</div>
+        <div className={style.dashboardContainer_light}>
+            <div className={style.header_light}>
+                <h1>DishDiary</h1>
+                <span>
+                    <p>Items</p>
+                    <p>Stats</p>
+                    <p>Profile</p>
+                </span>
+            </div>
+            <div className={style.dashboardBody_light}>
+                <div className={style.dashboardLeft_light}>
+                    <div className={style.leftHeader_light}>
+                        <h1>Schedule</h1>
+                    </div>
+                    <div className={style.leftBody_light}>
+
+                    </div>
+                </div>
+                <div className={style.dashboardRight_light}>
+                    <div className={style.rightHeader_light}>
+                        <h1>Content</h1>
+                    </div>
+                    <div className={style.rightBody_light}>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
